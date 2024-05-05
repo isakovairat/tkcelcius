eb834e1f1d57246994bb3942ebfb1ee9950567d9
Airat Isakov <isakov.airat@gmail.com>
2024-05-05 21:24:19 +0400

    release/1.0.0: fix[6]

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -13,35 +13,39 @@ import styles from "../main-nav.module.css"
 export const Footer = () => {
   return (
     <footer className="w-full border-t bg-[#466391] p-4 shadow md:flex md:items-center md:justify-between md:p-6">
-      <div className="mb-4 flex flex-col gap-2 md:mb-0">
-        <Link href="/" className="items-center space-x-2">
-          <span
-            className={cn(
-              "inline-block font-raleway text-2xl font-bold uppercase text-slate-50",
-              styles["main-nav-title-hover"]
-            )}
-          >
-            {siteConfig.name}
-          </span>
-        </Link>
-        <div>
-          <Link href={`tel:${siteConfig.phone}`}>
-            <span className="text-sm font-medium text-slate-50">
-              Телефон: <span className="font-bold">+7(968)-000-83-01</span>
+      <div className="mb-4 flex w-full flex-col md:mb-0">
+        <div className="mb-2 items-center">
+          <Link href="/" className="items-center space-x-2">
+            <span
+              className={cn(
+                "inline-block font-raleway text-2xl font-bold uppercase text-slate-50",
+                styles["main-nav-title-hover"]
+              )}
+            >
+              {siteConfig.name}
             </span>
           </Link>
         </div>
-        <div>
-          <Link href={`mailto:${siteConfig.email}`}>
-            <span className="text-sm font-medium text-slate-50">
-              E-mail: <span className="font-bold">{siteConfig.email}</span>
+        <div className="flex w-full flex-col items-center justify-center gap-2">
+          <div>
+            <Link href={`tel:${siteConfig.phone}`}>
+              <span className="text-sm font-medium text-slate-50">
+                Телефон: <span className="font-bold">+7(968)-000-83-01</span>
+              </span>
+            </Link>
+          </div>
+          <div>
+            <Link href={`mailto:${siteConfig.email}`}>
+              <span className="text-sm font-medium text-slate-50">
+                E-mail: <span className="font-bold">{siteConfig.email}</span>
+              </span>
+            </Link>
+          </div>
+          <div className="text-center">
+            <span className="text-balance text-sm font-medium text-slate-50">
+              Адрес: {siteConfig.address}
             </span>
-          </Link>
-        </div>
-        <div>
-          <span className="text-balance text-sm font-medium text-slate-50">
-            Адрес: {siteConfig.address}
-          </span>
+          </div>
         </div>
       </div>
       {/* {items?.length ? (
